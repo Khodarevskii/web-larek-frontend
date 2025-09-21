76c1eaff6be591e75a2232fb240ecbc62392207a
kirill <khodarevskii@yandex.ru>
2025-09-21 04:22:49 +0300

    закончил делать тесты

--- test/form.spec.js
+++ test/form.spec.js
@@ -4,6 +4,7 @@ const APP = "https://khodarevskii.github.io/web-larek-frontend/"
 const lead = {
   name: faker.name.firstName(),
   email: faker.internet.email(),
+  address: faker.address.streetAddress(),
   phone: faker.phone.phoneNumber(),
   message: faker.random.words()
 };
@@ -81,8 +82,18 @@ afterAll(() => {
     await expect(page.$(".basket__item")).resolves.toBe(null);
     }, 10000);
 
+     test("после удаление товара кнопка в крозине стала не доступна ", async () => {
+    await page.waitForSelector(".basket__button[disabled='disabled']")
+    await page.click(".modal__close");
+    }, 10000);
+
+
   test("В карточке можно нажать кнопку убрать товар ", async () => {
        await page.click(".card");
+      await page.waitForSelector(".card_full");
+       await page.click(".card__button");
+       await page.click(".card");
+        await page.waitForSelector(".card_full");
        await page.click(".card__button");
     }, 10000);
 
@@ -90,4 +101,63 @@ afterAll(() => {
       await expect(page.$eval(".header__basket-counter", el => el.innerText)).resolves.toBe('0');
     }, 10000);
 
+  test("после удаление товара он исчез из корзины", async () => {
+    await page.click(".header__basket");
+    await page.waitForSelector(".basket");
+    await expect(page.$(".basket__item")).resolves.toBe(null);
+    await page.click(".modal__close");
+    }, 10000);
+
+  test("при нажатие кнопки оформить в корзине появляется форма способа оплаты ", async () => {
+       await page.click(".card");
+       await page.waitForSelector(".card_full");
+       await page.click(".card__button");
+       await page.click(".header__basket");
+       await page.waitForSelector(".basket");
+       await page.click(".basket__button");
+       await page.waitForSelector("form");
+      }, 10000);
+
+    test("Кнопка внутри формы не доступна ", async () => {
+    await page.waitForSelector("[disabled='disabled']");
+    }, 10000);
+
+      test("пользователь может выбрать способ оплаты ", async () => {
+       await page.click(".button_alt");
+       await page.waitForSelector(".button_alt-active");
+      }, 10000);
+
+      test("пользователь может ввести совой адрес ", async () => {
+    await page.click("input[name=address]");
+    await page.type("input[name=address]", lead.address);
+      }, 10000);
+
+      test("после того как пользватель вел все данные кнопка стала доступна ", async () => {
+    await expect(page.$("[disabled='disabled']")).resolves.toBe(null);
+    }, 10000);
+
+
+    test("при нажатие кнопки далее в форме, появляеться  форма контакты ", async () => {
+    await page.click(".order__button");
+    await page.waitForSelector("form[name='contacts']");
+    }, 10000);
+
+      test("пользователь может ввести совой email ", async () => {
+    await page.click("input[name=email]");
+    await page.type("input[name=email]", lead.email);
+      }, 10000);
+
+      test("пользователь может ввести совой телефон ", async () => {
+    await page.click("input[name=phone]");
+    await page.type("input[name=phone]", lead.phone);
+      }, 10000);
+
+      test("после того как пользватель вел все данные кнопка стала доступна ", async () => {
+    await expect(page.$("[disabled='disabled']")).resolves.toBe(null);
+    }, 10000);
+
+    test("появляеться попап об успешной оплате ", async () => {
+    await page.click(".button");
+     await page.waitForSelector(".order-success");
+    }, 10000);
 
